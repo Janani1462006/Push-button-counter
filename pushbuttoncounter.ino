696ab5cedbfcf03a62c796a011f197314c6215b8
Janani1462006 <mjanani607@gmail.com>
2025-07-22 23:22:23 +0530

    Update pushbuttoncounter.ino

--- pushbuttoncounter.ino
+++ pushbuttoncounter.ino
@@ -1,1 +1,35 @@
+char command;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);  // Light (Red LED)
+  pinMode(9, OUTPUT);  // Fan (Green LED)
+  Serial.println("System Ready. Enter 1-4 to control devices.");
+}
+
+void loop() {
+  if (Serial.available()) {
+    command = Serial.read();
+
+    if (command == '1') {
+      digitalWrite(8, HIGH);   // Light ON
+      Serial.println("Light is ON");
+    }
+    else if (command == '2') {
+      digitalWrite(8, LOW);    // Light OFF
+      Serial.println("Light is OFF");
+    }
+    else if (command == '3') {
+      digitalWrite(9, HIGH);   // Fan ON
+      Serial.println("Fan is ON");
+    }
+    else if (command == '4') {
+      digitalWrite(9, LOW);    // Fan OFF
+      Serial.println("Fan is OFF");
+    }
+    else {
+      Serial.println("Invalid Command. Use 1-4.");
+    }
+  }
+}
 
